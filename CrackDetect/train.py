dc3bebc837da87c35837151349329d3f95caa00c
changyang-zeng66885 <zengcy@163.com>
2024-08-13 11:42:59 +0800

    Update

--- CrackDetect/train.py
+++ CrackDetect/train.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from sklearn.metrics import roc_auc_score
 from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import Compose, Resize, ToTensor
 from model import UNet
@@ -42,7 +43,7 @@ val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
 # Model, loss function, and optimizer
 model = UNet(in_channels=3, out_channels=1).to(device)
-# model.load_state_dict(torch.load("CrackDetect/model_saved/best_model_epoch_10.pth")) #加载保存的模型参数
+# model.load_state_dict(torch.load("CrackDetect/model_saved/best_model_imagesrcBig_20240808_epoch_30.pth")) #加载保存的模型参数
 criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -88,6 +89,8 @@ for epoch in range(num_epochs):
     # Validate
     model.eval()
     val_loss = 0.0
+    all_outputs = []
+    all_masks = []
     with torch.no_grad():
         for images, masks in val_loader:
             images = images.to(device)
@@ -97,11 +100,23 @@ for epoch in range(num_epochs):
             loss = criterion(outputs, masks)
             val_loss += loss.item()
 
+            # Convert logits to probabilities
+            probabilities = torch.sigmoid(outputs)
+            # Flatten tensors and collect results
+            all_outputs.append(probabilities.cpu().numpy().flatten())
+            all_masks.append(masks.cpu().numpy().flatten())
+
     val_loss /= len(val_loader)
 
+    # Concatenate all results
+    all_outputs = np.concatenate(all_outputs)
+    all_masks = np.concatenate(all_masks)
+    # Calculate AUC score
+    auc_score = roc_auc_score(all_masks, all_outputs)
+
     # Print training and validation loss
-    print(f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
-    wandb.log({"Train Loss": train_loss, "Val Loss": val_loss})
+    print(f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f} ,AUC: {auc_score:.4f}')
+    wandb.log({"Train Loss": train_loss, "Val Loss": val_loss,'AUC':auc_score})
 
 saveModelPath = f'CrackDetect/model_saved/best_model_imagesrcBig_epoch_{num_epochs}.pth'
 torch.save(model.state_dict(),saveModelPath )
